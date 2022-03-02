2eaf6a7e6f380b0d450163420d8d9f45f36ba91f
undertaker <emilfay90@gmail.com>
2022-03-02 13:46:38 +0300

    2 задача выполнена

--- learning_python/lesson_3.py
+++ learning_python/lesson_3.py
@@ -5,15 +5,33 @@
 >>> num_translate("eight")
 "восемь" """
 
-def num_translate(num):
-    print(english_nums.get(num.capitalize()))
+# def num_translate(num):
+#     print(english_nums.get(num.capitalize()))
+#
+#
+# english_nums = {"One": "Один", "Two": "Два", "Three": "Три", "Four": "Четыре", "Five": "Пять",
+#                 "Six": "Шесть", "Seven": "Семь", "Eight": "Восемь", "Nine": "Девять", "Ten": "Десять"}
+#
+# if __name__ == '__main__':
+#     while True:
+#         num = input("Программа переводящая числительные от 0 до 10 c английского на русский язык."
+#                 " Введите число на английском для перевода на русский: ")
+#         num_translate(num)
+
+"""Написать функцию thesaurus(), принимающую в качестве аргументов имена сотрудников и возвращающую словарь,
+ в котором ключи — первые буквы имён, а значения — списки, содержащие имена, начинающиеся с соответствующей буквы. """
 
 
-english_nums = {"One": "Один", "Two": "Два", "Three": "Три", "Four": "Четыре", "Five": "Пять",
-                "Six": "Шесть", "Seven": "Семь", "Eight": "Восемь", "Nine": "Девять", "Ten": "Десять"}
+def thesaurus(*args):
+    names_dict = {}
+    for i in sorted(args):
+        letter = i[0]
+        if letter in names_dict:
+            names_dict[letter] += [i]
+        else:
+            names_dict[letter] = [i]
+    return
 
-if __name__ == '__main__':
-    while True:
-        num = input("Программа переводящая числительные от 0 до 10 c английского на русский язык."
-                " Введите число на английском для перевода на русский: ")
-        num_translate(num)
+thesaurus("Иван", "Мария", "Владимир", "Максим", "Илья", "Макар","Дмитрий")
+
+
